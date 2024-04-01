5c54863987a47984c08e88039e82109596f17240
ikiwq <ikiigaiiii@gmail.com>
2024-04-01 16:20:48 +0200

    Fixed bugs in landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import CategoryCard from '@/components/category/categoryCard/CategoryCard';
 export default async function Home() {
   let recentPosts = await getUnmarkedRecentArticles(15);
 
-  let categories = await getCategories();
+  let categoriesCollection = await getCategories();
   let featuredArticles = await getFeatured(5);
 
   return (
@@ -25,12 +25,12 @@ export default async function Home() {
       </div>
       <div className='flex flex-col w-full sticky top-0 lg:h-80 flex-shrink-0 lg:w-3/10'>
         {
-          categories && categories?.length > 0 && (
+          categoriesCollection && categoriesCollection?.count > 0 && (
             <div className='flex flex-col gap-1'>
               <h1 className='text-lg font-bold text-red-600'>POPULAR CATEGORIES</h1>
               <div className='flex flex-wrap gap-2 pb-2'>
                 {
-                  categories.map((category, index) => {
+                  categoriesCollection.categories.map((category, index) => {
                     return (
                       <CategoryCard key={"popular-category-" + index} category={category} />
                     )
